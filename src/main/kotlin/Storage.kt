2f0e5450176b032003db8e3cf393950091adfdfa
Kenta Kase <kesin1202000@gmail.com>
2020-12-29 23:35:20 +0900

    store supports multiple files

--- src/main/kotlin/Storage.kt
+++ src/main/kotlin/Storage.kt
@@ -1,8 +1,11 @@
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.Bucket
 import com.google.cloud.storage.StorageOptions
+import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Collectors.toList
 
 class Storage(bucketPath: String) {
     private var bucket: Bucket
@@ -13,10 +16,9 @@ class Storage(bucketPath: String) {
     }
 
     // TODO: result型を返したい
-    // TODO: 複数ファイル対応
-    fun store(localPath: String, key: String, tags: List<String>) {
-        val blob = storeBlobs(localPath)
-        storeIndex(blob.name, key, tags)
+    fun store(pathsOrGlob: List<String>, key: String, tags: List<String>) {
+        val blobs = storeBlobs(pathsOrGlob)
+        storeIndex(blobs, key, tags)
     }
 
     // TODO: 複数ファイル対応
@@ -25,7 +27,8 @@ class Storage(bucketPath: String) {
         downloadBlobs(remotePath, localPath)
     }
 
-    private fun storeIndex(remotePath: String, key: String, tags: List<String>) {
+    private fun storeIndex(blobs: List<Blob>, key: String, tags: List<String>) {
+        val remotePath = blobs.map { it.name }.joinToString(separator = "\n")
         val indexPaths = tags.map { "skw_index/$key/$it" }
         indexPaths.forEach {
             bucket.create(it, remotePath.toByteArray())
@@ -38,17 +41,47 @@ class Storage(bucketPath: String) {
         return String(blob.getContent())
     }
 
-    private fun storeBlobs(localPath: String): Blob {
-        val filePath = Paths.get(localPath)
-        val blobName = toBlobName(localPath)
-        val blobInputStream = Files.newInputStream(filePath)
+    private fun storeBlobs(pathsOrGlob: List<String>): List<Blob> {
+        val paths = globMatchedPaths(pathsOrGlob)
+        println(paths.map { it.toString() })
+        // TODO: 1つもマッチしなかった場合はエラーを出しておく
+        return paths.map { localPath ->
+            val blobName = toBlobName(localPath)
+            val blobInputStream = Files.newInputStream(localPath)
+            return@map bucket.create(blobName, blobInputStream)
+        }
+    }
 
-        return bucket.create(blobName, blobInputStream)
+    // パスの配列かglobの2パターンをList<Path>に正規化する
+    // CLIのオプションにファイルパスを渡す場合、"*"などのワイルドカードはシェルがパスを複数の引数に展開してくる
+    // "**/*"などシェルが展開できなかった場合は単純に文字列となる
+    // この違いを吸収する
+    private fun globMatchedPaths(pathsOrGlob: List<String>): List<Path> {
+        // シェルがパスを展開したケース
+        if (pathsOrGlob.size > 1) {
+           return pathsOrGlob.map { Paths.get(it).normalize() }
+        }
+
+        // 引数がglobの文字列として解釈されたケース
+        val glob = pathsOrGlob.first()
+        val fileSystems = FileSystems.getDefault()
+        // wildcardが含まれているとPaths.getがパースできないので独自にLinux, Windowsのパスに対応するglobに書き換える
+        val globStrings = glob.split("/")
+
+        // walkのコストを抑えるため、ワイルドカードより1つ上のディレクトリをrootにする
+        val rootDirStrings = globStrings.map { it }.takeWhile { !it.contains("*") }
+        val rootDirPath = Paths.get(".", *rootDirStrings.toTypedArray()).normalize()
+
+        val matcher = fileSystems.getPathMatcher("glob:$glob")
+        return Files.walk(rootDirPath)
+            .filter(matcher::matches)
+            .collect(toList())
+            .toList()
     }
 
     // ./sample/text.txt -> sample/text.txtのようにGCS上のパスに合うように変換する
-    private fun toBlobName(localPath: String): String {
-        val filePath = Paths.get(localPath).normalize()
+    private fun toBlobName(localPath: Path): String {
+        val filePath = localPath.normalize()
         // Windowsでのバックスラッシュを変換するために手動でjoinする
         return filePath.toList().joinToString(separator = "/")
     }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,14 +3,15 @@ import kotlinx.cli.*
 fun main(args: Array<String>) {
     val parser = ArgParser("skw")
     class StoreCommand: Subcommand("store", "Store subcommand") {
-        val path: String by argument(ArgType.String, description = "Path")
+        val pathsOrGlob: List<String> by argument(ArgType.String, description = "Paths or file glob").multiple(10000)
         val bucketPath: String by option(ArgType.String, shortName = "b", description = "GCS bucket name").required()
         val key: String by option(ArgType.String, shortName = "k", description = "Key").required()
         val tags: List<String> by option(ArgType.String, shortName = "t", description = "Tags").multiple() // defaultでlatestにしたいがやり方がわからない
         override fun execute() {
+            println(pathsOrGlob)
             // Upload
             val storage = Storage(bucketPath)
-            storage.store(path, key, tags)
+            storage.store(pathsOrGlob, key, tags)
         }
     }
     class GetCommand: Subcommand("get", "Get subcommand") {
