fead586e8d8d1d96749bc1a77c2d06af57808af7
Kenta Kase <kesin1202000@gmail.com>
2021-01-10 14:48:35 +0900

    chore: Refactor tests

--- src/test/kotlin/StorageTest.kt
+++ src/test/kotlin/StorageTest.kt
@@ -10,8 +10,9 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 internal class StorageTest {
-    lateinit var bucketMock: Bucket
-    lateinit var blobMock: Blob
+    private lateinit var bucketMock: Bucket
+    private lateinit var blobMock: Blob
+    private lateinit var storage: Storage
 
     @BeforeEach
     fun setUp() {
@@ -24,98 +25,90 @@ internal class StorageTest {
         every { blobMock.getContent() } returns "".toByteArray()
 
         bucketMock = mockk()
+        storage = Storage(bucketMock)
     }
 
     @AfterEach
     fun tearDown() {
     }
 
-    @Test
-    fun storeWhenSuccess() {
-        val pathsOrGlob = listOf("./sample")
-        val prefix = "."
-        val key = "test"
-        val tags = listOf("latest")
-        mockkStatic(::getLocalFilePaths)
-        every { bucketMock.create(any<String>(), any<ByteArray>()) } returns blobMock
-        every { bucketMock.create(any<String>(), any<InputStream>()) } returns blobMock
-        every { getLocalFilePaths(any()) } returns listOf(Paths.get(""))
+    @Nested
+    inner class TestStore {
+        private val pathsOrGlob = listOf("./sample")
+        private val prefix = "."
+        private val key = "test"
+        private val tags = listOf("latest")
 
-        val storage = Storage(bucketMock)
-        assertDoesNotThrow { storage.store(pathsOrGlob, key, tags, prefix) }
-    }
+        @BeforeEach
+        fun setUp() {
+            mockkStatic(::getLocalFilePaths)
+        }
 
-    @Test
-    fun storeWhenLocalFileNotFound() {
-        val pathsOrGlob = listOf("./sample")
-        val prefix = "."
-        val key = "test"
-        val tags = listOf("latest")
-        val dummyPath = Paths.get("nothing.txt")
-        mockkStatic(::getLocalFilePaths)
-        every { getLocalFilePaths(any()) } returns listOf(dummyPath)
-        every { Files.newInputStream(dummyPath) } throws IOException()
+        @Test
+        fun whenSuccess() {
+            every { bucketMock.create(any<String>(), any<ByteArray>()) } returns blobMock
+            every { bucketMock.create(any<String>(), any<InputStream>()) } returns blobMock
+            every { getLocalFilePaths(any()) } returns listOf(Paths.get(""))
 
-        val storage = Storage(bucketMock)
-        assertThrows<IOException> {
-            storage.store(pathsOrGlob, key, tags, prefix)
+            assertDoesNotThrow { storage.store(pathsOrGlob, key, tags, prefix) }
+        }
+
+        @Test
+        fun whenLocalFileNotFound() {
+            val dummyPath = Paths.get("nothing.txt")
+            every { getLocalFilePaths(any()) } returns listOf(dummyPath)
+            every { Files.newInputStream(dummyPath) } throws IOException()
+
+            assertThrows<IOException> {
+                storage.store(pathsOrGlob, key, tags, prefix)
+            }
+        }
+
+        @Test
+        fun whenFailUploadToRemote() {
+            every { getLocalFilePaths(any()) } returns listOf(Paths.get("success.txt"), Paths.get("fail.txt"))
+            every { bucketMock.create("success.txt", any<InputStream>()) } returns blobMock
+            every { bucketMock.create("fail.txt", any<InputStream>()) } throws StorageException(500, "Dummy Error")
+
+            assertThrows<StorageException> {
+                storage.store(pathsOrGlob, key, tags, prefix)
+            }
         }
     }
 
-    @Test
-    fun storeWhenFailStoreBlobs() {
-        val pathsOrGlob = listOf("./sample")
-        val prefix = "."
-        val key = "test"
-        val tags = listOf("latest")
-        mockkStatic(::getLocalFilePaths)
-        every { getLocalFilePaths(any()) } returns listOf(Paths.get("success.txt"), Paths.get("fail.txt"))
-        every { bucketMock.create("success.txt", any<InputStream>()) } returns blobMock
-        every { bucketMock.create("fail.txt", any<InputStream>()) } throws StorageException(500, "Dummy Error")
+    @Nested
+    inner class TestDownload {
+        private val localPath = "test"
+        private val key = "test"
+        private val tag = "latest"
 
-        val storage = Storage(bucketMock)
-        assertThrows<StorageException> {
-            storage.store(pathsOrGlob, key, tags, prefix)
+        @Test
+        fun whenSuccess() {
+            every { bucketMock.get(any<String>()) } returns blobMock
+            justRun { blobMock.downloadTo(any<Path>()) }
+
+            assertDoesNotThrow { storage.download(localPath, key, tag) }
         }
-    }
 
-    @Test
-    fun downloadWhenSuccess() {
-        every { bucketMock.get(any<String>()) } returns blobMock
-        justRun { blobMock.downloadTo(any<Path>()) }
-        val localPath = "test"
-        val key = "test"
-        val tag = "latest"
+        @Test
+        fun whenFailFetchIndex() {
+            every { bucketMock.get("skw_index/$key/$tag") } throws StorageException(500, "Dummy Error")
 
-        val storage = Storage(bucketMock)
-        assertDoesNotThrow { storage.download(localPath, key, tag) }
-    }
+            assertThrows<StorageException> { storage.download(localPath, key, tag) }
+            // Assert does not download blob files when failed to fetch index.
+            verify(inverse = true) { blobMock.downloadTo(any<Path>()) }
+        }
 
-    @Test
-    fun downloadWhenFailFetchIndex() {
-        val localPath = "test"
-        val key = "test"
-        val tag = "latest"
-        every { bucketMock.get("skw_index/$key/$tag") } throws StorageException(500, "Dummy Error")
+        @Test
+        fun whenFailDownloadFromRemote() {
+            val indexBlobMock = mockk<Blob>()
+            every { bucketMock.get("skw_index/$key/$tag") } returns indexBlobMock
+            every { indexBlobMock.getContent() } returns "success.txt\nfail.txt".toByteArray()
+            every { bucketMock.get("success.txt") } returns blobMock
+            every { bucketMock.get("fail.txt") } throws StorageException(500, "Dummy Error")
+            justRun { blobMock.downloadTo(any<Path>()) }
 
-        val storage = Storage(bucketMock)
-        assertThrows<StorageException> { storage.download(localPath, key, tag) }
-        verify(inverse = true) { blobMock.downloadTo(any<Path>()) }
-    }
-
-    @Test
-    fun downloadWhenFailDownloadBlobs() {
-        val localPath = "test"
-        val key = "test"
-        val tag = "latest"
-        val indexBlobMock = mockk<Blob>()
-        every { bucketMock.get("skw_index/$key/$tag") } returns indexBlobMock
-        every { indexBlobMock.getContent() } returns "success.txt\nfail.txt".toByteArray()
-        every { bucketMock.get("success.txt") } returns blobMock
-        every { bucketMock.get("fail.txt") } throws StorageException(500, "Dummy Error")
-        justRun { blobMock.downloadTo(any<Path>()) }
-
-        val storage = Storage(bucketMock)
-        assertThrows<StorageException> { storage.download(localPath, key, tag) }
+            assertThrows<StorageException> { storage.download(localPath, key, tag) }
+        }
     }
 }
