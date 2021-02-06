f589739db46b1583ad074cbf0074dcc459d3a3b5
Kenta Kase <kesin1202000@gmail.com>
2021-02-06 11:44:01 +0900

    Enable dependency lock

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("io.perezalcolea.gcs-build-cache") version "0.1.0"
 }
 
+enableFeaturePreview("ONE_LOCKFILE_PER_PROJECT")
+
 buildCache {
     val isCi = System.getenv().containsKey("CI")
     local {
