a6ea0d1bf6c300050a9a3c2c73f268f394f5e293
Kenta Kase <kesin1202000@gmail.com>
2021-03-07 21:15:35 +0900

    chore: Fix shipjs config

--- ship.config.js
+++ ship.config.js
@@ -1,16 +1,18 @@
 module.exports = {
   // prepare phase
-  installCommand: ({ isYarn }) => undefined, // disable
+  installCommand: ({ isYarn }) => 'true', // disable
   versionUpdated: ({ version, releaseType, dir, exec }) => {
     // Update version var inside gradle script
-    exec(`sed -iE "s/version = .+/version = \"${version}\"/" build.gradle.kts`)
+    exec(`sed -i -E 's/version = .+/version = "${version}"/' build.gradle.kts`)
   },
   // trigger phase
   buildCommand: () => './gradlew assembleArchive',
-  publishCommand: () => '', // disable
-  releases: [
-    'build/libs/skw.jar',
-    'build/distributions/skw.tar',
-    'build/distributions/skw.zip'
-  ]
+  publishCommand: () => 'true', // disable
+  releases: {
+    assetsToUpload: [
+      'build/libs/skw.jar',
+      'build/distributions/skw.tar',
+      'build/distributions/skw.zip'
+    ]
+  }
 };
