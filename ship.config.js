a3ebe21205d32b34a91bb1ab672c2e8448376d2b
Kenta Kase <kesin1202000@gmail.com>
2021-03-07 22:20:03 +0900

    fix: Disable build in shipjs trigger

--- ship.config.js
+++ ship.config.js
@@ -6,13 +6,6 @@ module.exports = {
     exec(`sed -i -E 's/version = .+/version = "${version}"/' build.gradle.kts`)
   },
   // trigger phase
-  buildCommand: () => './gradlew assembleArchive',
+  buildCommand: () => '.true', // disable
   publishCommand: () => 'true', // disable
-  releases: {
-    assetsToUpload: [
-      'build/libs/skw.jar',
-      'build/distributions/skw.tar',
-      'build/distributions/skw.zip'
-    ]
-  }
 };
